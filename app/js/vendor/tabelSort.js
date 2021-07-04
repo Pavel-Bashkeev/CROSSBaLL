ae0fe551aa66ebb1d9859c021ec59a84bc666fdc
Pavel-Bashkeev <pavelbashkeev@gmail.com>
2021-07-04 23:17:36 +0300

    Сделана сортировка таблицы

--- app/js/vendor/tabelSort.js
+++ app/js/vendor/tabelSort.js
@@ -1,4 +1,4 @@
-const myArrayTable = [
+let myArrayTable = [
 	{ 'nameSesson': 'Сезон 2021/2020', 'countGame': '5', 'gol': '9', 'pas': '2', 'golPas': '1', 'rating': '90' },
 	{ 'nameSesson': 'Сезон 2020/2019', 'countGame': '4', 'gol': '8', 'pas': '5', 'golPas': '2', 'rating': '97' },
 	{ 'nameSesson': 'Сезон 2019/2018', 'countGame': '3', 'gol': '7', 'pas': '3', 'golPas': '3', 'rating': '30' },
@@ -8,9 +8,27 @@ const myArrayTable = [
 
 buildTabel(myArrayTable);
 
-function buildTabel(data) {
-	const tbody = document.querySelector('.table-body');
+const nameTHead = document.querySelectorAll('.table-head__sort');
+const tableHead = document.querySelector('.table-head');
 
+nameTHead.forEach(item => {
+	item.addEventListener('click', function () {
+		let columnName = this.getAttribute('data-column');
+		let orderColumn = this.getAttribute('data-order');
+		if (orderColumn == 'descr') {
+			item.setAttribute('data-order', 'asc');
+			myArrayTable = myArrayTable.sort((a, b) => a[columnName] > b[columnName] ? 1 : -1);
+		} else {
+			item.setAttribute('data-order', 'descr');
+			myArrayTable = myArrayTable.sort((a, b) => a[columnName] < b[columnName] ? 1 : -1);
+		}
+		buildTabel(myArrayTable);
+	})
+})
+
+function buildTabel(data) {
+	let tbody = document.querySelector('.table-body');
+	tbody.innerHTML = '';
 	for (let i = 0; i < data.length; i++) {
 		let row = `
 		<tr class='table-body__row'>
